3cfa899c99c5f95fd02277bf52758166ecfbf34d
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2023-07-27 21:53:55 -0300

    feat: models

---
+++ server/src/models/CsvLine.ts
@@ -0,0 +1,14 @@
+export type CsvLine = {
+	csv_id: string
+	name: string
+	city: string
+	country: string
+	favorite_sport: string
+}
+
+export type CsvLineDTO = {
+	name: string
+	city: string
+	country: string
+	favorite_sport: string
+}
