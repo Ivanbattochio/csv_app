1f8b4c749de7ba15ad82f4bfca96b2c63e2e78f3
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2023-07-27 22:30:02 -0300

    feat: user controller and route

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -3,6 +3,6 @@ import { get } from "../controllers/userController"
 
 const userRouter = express.Router()
 
-userRouter.get("/users", (req, res) => {})
+userRouter.get("/users", get)
 
 export default userRouter
