f3d37b2cef2d5e461433500dd73e110149313bfa
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2023-07-29 12:56:32 -0300

    feat: input button functionality and notification error when more than one file is selected at once

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,43 @@
-import { Container, Typography, TextField, Box, Button } from '@mui/material'
-import { useState } from 'react'
+import { Container, Typography, TextField, Box, Button, Snackbar, Alert } from '@mui/material'
+import { SyntheticEvent, useState } from 'react'
 import { CsvLine } from './models/CsvLine'
 import { CsvDataComponent } from './components/CsvDataComponent'
 
 function App() {
     const [paginatedCsvData, paginatedCsvDataSet] = useState<CsvLine[]>([])
+
+    const [notificationOpen, notificationOpenSet] = useState<boolean>(false)
+    const [alertMessage, alertMessageSet] = useState<string>('')
+    const [alertType, alertTypeSet] = useState<'warning' | 'info' | 'success' | 'error'>('info')
+
+    const openNotification = (message: string, type: 'warning' | 'info' | 'success' | 'error') => {
+        alertMessageSet(message)
+        alertTypeSet(type)
+        notificationOpenSet(true)
+    }
+
+    const closeSnackBar = () => {
+        alertMessageSet('')
+        notificationOpenSet(false)
+    }
+
+    const removeLastFilePath = (event: SyntheticEvent) => {
+        const target = event.target as HTMLInputElement
+        target.value = ''
+    }
+
+    const uploadCsv = (event: SyntheticEvent) => {
+        const target = event.target as HTMLInputElement
+        const filesArray = target.files
+
+        if (!filesArray) return
+        if (!filesArray.length) return
+
+        if (filesArray.length > 1) {
+            openNotification('You can only submit one CSV at a time!', 'error')
+            return
+        }
+    }
 
     return (
         <Container fixed sx={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '30px' }}>
@@ -20,7 +53,15 @@ function App() {
                 />
                 <CsvDataComponent paginatedData={paginatedCsvData}></CsvDataComponent>
 
-                <input accept="image/*" style={{ display: 'none' }} id="raised-button-file" multiple type="file" />
+                <input
+                    accept=".csv"
+                    style={{ display: 'none' }}
+                    id="raised-button-file"
+                    multiple
+                    type="file"
+                    onChange={uploadCsv}
+                    onClick={removeLastFilePath}
+                />
                 <label htmlFor="raised-button-file">
                     <Button
                         variant="contained"
@@ -36,6 +77,10 @@ function App() {
                     </Button>
                 </label>
             </Box>
+
+            <Snackbar open={notificationOpen} autoHideDuration={6000} onClose={closeSnackBar} message={alertMessage}>
+                <Alert severity={alertType}>{alertMessage}</Alert>
+            </Snackbar>
         </Container>
     )
 }
