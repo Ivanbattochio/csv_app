8755bfa3bd07574f82364d9609a688c3e097bf30
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2023-07-29 17:58:39 -0300

    feat: feedback from file insertion, and new states for file tracking

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,17 @@ import axios from 'axios'
 import { Container, Typography, TextField, Box, Button, Snackbar, Alert, CircularProgress } from '@mui/material'
 import FileUploadIcon from '@mui/icons-material/FileUpload'
 import { SyntheticEvent, useState } from 'react'
-import { CsvLine } from './models/CsvLine'
+import { CsvLine, PostFileResponse } from './models/CsvLine'
 import { CsvDataComponent } from './components/CsvDataComponent'
 
 function App() {
     const [paginatedCsvData, paginatedCsvDataSet] = useState<CsvLine[]>([])
 
     const [loadingCsv, loadingCsvSet] = useState<boolean>(false)
+
+    const [fileId, fileIdSet] = useState<string>('')
+
+    const [textField, textFieldSet] = useState<string>('')
 
     const [notificationOpen, notificationOpenSet] = useState<boolean>(false)
     const [alertMessage, alertMessageSet] = useState<string>('')
@@ -31,6 +35,13 @@ function App() {
         target.value = ''
     }
 
+    const updateTextFieldState = (event: SyntheticEvent) => {
+        const target = event.target as HTMLInputElement
+        textFieldSet(target.value)
+    }
+
+    const fetchFileData = () => {}
+
     const uploadCsv = (event: SyntheticEvent) => {
         const target = event.target as HTMLInputElement
         const filesArray = target.files
@@ -50,9 +61,19 @@ function App() {
         loadingCsvSet(true)
 
         axios
-            .post('http://localhost:3000/api/files', formData)
+            .post<PostFileResponse>('http://localhost:3000/api/files', formData)
             .then((res) => {
-                console.log(res)
+                const { fileId, successfullyInsertedLinesQuantity, wronglyFormattedLinesIndex } = res.data
+                if (wronglyFormattedLinesIndex.length) {
+                    openNotification(
+                        `${successfullyInsertedLinesQuantity} line(s) were successfully inserted. The following lines were wrongly formatted: ${wronglyFormattedLinesIndex}`,
+                        'warning'
+                    )
+                } else {
+                    openNotification(`${successfullyInsertedLinesQuantity} line(s) were successfully inserted`, 'success')
+                }
+                fileIdSet(fileId)
+                fetchFileData()
             })
             .catch((error) => {
                 console.log(error)
@@ -67,13 +88,17 @@ function App() {
             <Typography sx={{ fontWeight: '600' }} color="#ACA9BB" fontSize={50}>
                 CSV UPLOADER
             </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', width: '100%' }}>
-                <TextField
-                    id="outlined-basic"
-                    label="Search CSV Fields"
-                    variant="outlined"
-                    sx={{ backgroundColor: '#ACA9BB', borderRadius: '10px', width: '60%' }}
-                />
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', width: '100vh' }}>
+                <form onSubmit={fetchFileData}>
+                    <TextField
+                        id="outlined-basic"
+                        label="Search CSV Fields"
+                        variant="outlined"
+                        value={textField}
+                        sx={{ backgroundColor: '#ACA9BB', borderRadius: '10px', width: '100%' }}
+                        onChange={updateTextFieldState}
+                    />
+                </form>
                 <CsvDataComponent paginatedData={paginatedCsvData}></CsvDataComponent>
 
                 <input
